2a9a03af5d0b1c29cc479f941e5ce9c2eb59498f
gloriayordanova <raevagloria@gmail.com>
2024-07-20 10:29:23 +0100

    Final Version

--- 1. Colors/script.js
+++ 1. Colors/script.js
@@ -1,10 +1,10 @@
 var index = 0;
 
-function changeColors(){
-var colors = ["purple", "green", "blue", "orange", "yellow"];
+function changeColors() {
+    var colors = ["green", "purple", "red", "yellow", "blue"];
 
-document.getElementsByTagName("body")[0].style.backgroundColor = colors[index++];
+    document.getElementsByTagName("body")[0].style.background = colors[index++];
 
-if(index > colors.length - 1) 
+    if(index > colors.length - 1) 
     index = 0;
 }

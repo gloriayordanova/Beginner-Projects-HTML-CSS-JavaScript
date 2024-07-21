54a55caefb1b88f2a845c317e3036e5a971fcaa1
gloriayordanova <raevagloria@gmail.com>
2024-07-21 13:01:35 +0100

    Final Version

--- 1. Colors/script.js
+++ 1. Colors/script.js
@@ -1,29 +1,29 @@
 var index = 0; 
+var colors = ["green", "purple", "red", "yellow", "blue"];
+
+function callColors() {
+    document.body.style.background = colors[index];
+}
 
 function changeColors1() {
-    var colors = ["green", "purple", "red", "yellow", "blue"];
-
-    document.getElementsByTagName("body")[0].style.background = colors[index++];
-   
+    callColors();
+    
+    index++;
     if (index > colors.length - 1) 
         index = 0;
 }
 
 function changeColors2() {
-    var colors = ["green", "purple", "red", "yellow", "blue"];
-
     index--;
     if (index < 0)
         index = colors.length - 1;
 
-    document.getElementsByTagName("body")[0].style.background = colors[index];
+    callColors();
 
 }
 
 function clearColors() {
-    // Reset the background color to its original state
-    document.body.style.backgroundColor = "white"; // Set to the initial state
+    document.body.style.backgroundColor = "white"; 
 
-    // If there are other things to reset, add them here
-    index = 0; // Reset the color index if needed
+    index = 0; 
 }

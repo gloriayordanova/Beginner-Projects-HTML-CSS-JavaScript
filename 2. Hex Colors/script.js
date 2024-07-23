c8037bb0db6e988a12583584d788a0aab32b4c26
gloriayordanova <raevagloria@gmail.com>
2024-07-23 12:34:28 +0100

    Final Version

--- 2. Hex Colors/script.js
+++ 2. Hex Colors/script.js
@@ -1,13 +1,13 @@
-function changeColor(){
-    var hexNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8",  "9", "A", "B", "C", "D", "E", "F"];
+function changeColor() {
+    var hexNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 
     var hexCode = '';
 
-    for(let i = 0; i < 6; i++) {
-        var randomIndex = Math.floor(Math.random() * hexNumbers.length);
+    for(var i = 0; i < 6; i++){
+    var randomIndex = Math.floor(Math.random() * hexNumbers.length);
 
-        hexCode += hexNumbers[randomIndex];
+    hexCode += hexNumbers[randomIndex];
     }
-    document.getElementById("hex-code").innerHTML = hexCode;
-    document.getElementsByTagName("body")[0].style.background = "#" + hexCode;
+    document.getElementById("hex-figure").innerHTML = hexCode;
+    document.body.style.background = "#" + hexCode;
 }

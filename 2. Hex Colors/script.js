7e91de68a5ffbbf441ad0192cd4d099361d8509e
gloriayordanova <raevagloria@gmail.com>
2024-07-24 12:45:54 +0100

    Final Verison

--- 2. Hex Colors/script.js
+++ 2. Hex Colors/script.js
@@ -22,7 +22,34 @@ function changeColorButton() {
 
     hexCode += hexNumbers[randomIndex];
     }
-    var button = document.querySelector('.change-color-button');
-    button.style.backgroundColor = "#" + hexCode;
+   
     document.getElementById("hex-figure").innerHTML = hexCode;
+    var button = document.querySelector(".change-color-button");
+    button.style.backgroundColor = "#" + hexCode;
+
 }
+
+// function generateHexColor() {
+//     const hexNumbers = "0123456789ABCDEF";
+//     let hexCode = '';
+
+//     for (let i = 0; i < 6; i++) {
+//         const randomIndex = Math.floor(Math.random() * hexNumbers.length);
+//         hexCode += hexNumbers[randomIndex];
+//     }
+
+//     return hexCode;
+// }
+
+// function changeColor() {
+//     const hexCode = generateHexColor();
+//     document.getElementById("hex-figure").innerHTML = hexCode;
+//     document.body.style.background = `#${hexCode}`;
+// }
+
+// function changeColorButton() {
+//     const hexCode = generateHexColor();
+//     const button = document.querySelector('.change-color-button');
+//     button.style.backgroundColor = `#${hexCode}`;
+//     document.getElementById("hex-figure").innerHTML = hexCode;
+// }

866dd0b7e9192e85678d95b82baac6d5bd87aebe
gloriayordanova <raevagloria@gmail.com>
2024-07-24 13:11:21 +0100

    Final Version

--- 2. Hex Colors/script.js
+++ 2. Hex Colors/script.js
@@ -1,54 +1,23 @@
-function changeColor() {
-    var hexNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+function generateHexColor() {
+    const hexNumbers = "0123456789ABCDEF";
+    let hexCode = '';
 
-    var hexCode = '';
-
-    for(var i = 0; i < 6; i++){
-    var randomIndex = Math.floor(Math.random() * hexNumbers.length);
-
-    hexCode += hexNumbers[randomIndex];
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * hexNumbers.length);
+        hexCode += hexNumbers[randomIndex];
     }
+
+    return hexCode;
+}
+
+function changeColor() {
+    const hexCode = generateHexColor();
     document.getElementById("hex-figure").innerHTML = hexCode;
-    document.body.style.background = "#" + hexCode;
+    document.body.style.background = `#${hexCode}`;
 }
 
 function changeColorButton() {
-    var hexNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
-
-    var hexCode = '';
-
-    for(var i = 0; i < 6; i++){
-    var randomIndex = Math.floor(Math.random() * hexNumbers.length);
-
-    hexCode += hexNumbers[randomIndex];
-    }
-   
+    const hexCode = generateHexColor();
     document.getElementById("hex-figure").innerHTML = hexCode;
-    document.querySelector(".change-color-button").style.backgroundColor = "#" + hexCode;
-
+    document.querySelector('.change-color-button').style.backgroundColor = `#${hexCode}`;
 }
-
-// function generateHexColor() {
-//     const hexNumbers = "0123456789ABCDEF";
-//     let hexCode = '';
-
-//     for (let i = 0; i < 6; i++) {
-//         const randomIndex = Math.floor(Math.random() * hexNumbers.length);
-//         hexCode += hexNumbers[randomIndex];
-//     }
-
-//     return hexCode;
-// }
-
-// function changeColor() {
-//     const hexCode = generateHexColor();
-//     document.getElementById("hex-figure").innerHTML = hexCode;
-//     document.body.style.background = `#${hexCode}`;
-// }
-
-// function changeColorButton() {
-//     const hexCode = generateHexColor();
-//     const button = document.querySelector('.change-color-button');
-//     button.style.backgroundColor = `#${hexCode}`;
-//     document.getElementById("hex-figure").innerHTML = hexCode;
-// }

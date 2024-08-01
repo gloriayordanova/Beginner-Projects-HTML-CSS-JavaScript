6f20b0188f49149e5e58ce61b100b43f763ca0e8
gloriayordanova <raevagloria@gmail.com>
2024-08-01 10:36:53 +0100

    Final Version

--- 4. Pass the Message/script.js
+++ 4. Pass the Message/script.js
@@ -1,12 +1,25 @@
 const messageInput = document.getElementById("message-input");
 
-messageInput.addEventListener("keydown", function(event){
-    if(event.key == "Enter"){
-        getMessage();
+messageInput.addEventListener("keydown", function(event) {
+    if(event.key == "Enter") {
+        getMessageUpperCase();
     }
 });
 
-function getMessage(){
-    document.getElementById("message-output").innerHTML = messageInput.value;
+function getMessageUpperCase() {
+    document.getElementById("message-output-upper-case").innerHTML = messageInput.value;
+    document.getElementById("message-output-lower-case").innerHTML = ""; 
+    messageInput.value = '';
+}
+
+messageInput.addEventListener("keydown", function(event) {
+    if(event.key == "Escape") {
+        getMessageLowerCase();
+    }
+});
+
+function getMessageLowerCase() {
+    document.getElementById("message-output-lower-case").innerHTML = messageInput.value;
+    document.getElementById("message-output-upper-case").innerHTML = ""; 
     messageInput.value = '';
 }

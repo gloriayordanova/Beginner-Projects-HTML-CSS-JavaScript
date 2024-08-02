6827db2e90eaee960c916bc0ac109a0781db3a19
gloriayordanova <raevagloria@gmail.com>
2024-08-02 10:41:08 +0100

    Final Version

--- 5. Counter/script.js
+++ 5. Counter/script.js
@@ -1,6 +1,23 @@
 let count = 0;
 
-function changeCount(num){
-    count += num;
+function changeCount(num, operation){
+    if (operation === 'multiply') {
+        count *= 2;
+    } else if (operation === 'divide') {
+            count /= 2;
+    } else if (operation === 'clear') {
+        count = 0;
+    } else {
+        count += num;
+    }
     document.getElementById("count").innerHTML = count;
 }
+
+
+
+// let count = 0;
+
+// function changeCount(num){
+//     count += num;
+//     document.getElementById("count").innerHTML = count;
+// }

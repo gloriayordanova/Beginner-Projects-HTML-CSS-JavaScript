67851daabf80399b643fc310e9058f4dd7611526
gloriayordanova <raevagloria@gmail.com>
2024-08-04 09:56:40 +0100

    Final Version

--- 6. Image Carousel/script.js
+++ 6. Image Carousel/script.js
@@ -13,7 +13,8 @@ function show_image(i){
     }
 
     for(let i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active", ""); //This loop removes the " active" class from all dots.
+        //This ensures that only one dot will be highlighted at a time. By replacing "active" with an empty string, it effectively removes the active class if it exists.;
     }
 
     if(index > images.length - 1){
@@ -25,5 +26,6 @@ function show_image(i){
     }
 
     images[index].style.display = "block";
-    dots[index].className += " active";
+    dots[index].className += " active"; //After determining which image should be shown (based on the index variable),
+    //this line adds the " active" class to the corresponding dot. The += operator appends the " active" class to the current class list of that dot, which highlights it.
 }

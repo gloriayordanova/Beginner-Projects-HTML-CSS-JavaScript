95b51b1cd71b5176d0f36bc8691325c5ba116ea5
gloriayordanova <raevagloria@gmail.com>
2024-08-07 12:56:48 +0100

    Final Version

--- 6. Image Carousel/script.js
+++ 6. Image Carousel/script.js
@@ -27,3 +27,36 @@ function show_image(i){
     images[index].style.display = "block";
     dots[index].className += " active";
 }
+
+
+
+
+// let index = 0;
+
+// show_image(index);
+
+// function show_image(i){
+//     index += i;
+
+//     let images = document.getElementsByClassName("image");
+//     let dots = document.getElementsByClassName("dot");
+
+//     for(let i = 0; i < images.length; i++){
+//         images[i].style.display = "none";
+//     }
+
+//     for(let i = 0; i < dots.length; i++){
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+
+//     if(index > images.length - 1){
+//         index = 0;
+//     }
+
+//     if(index < 0){
+//         index = images.length - 1;
+//     }
+
+//     images[index].style.display = "block";
+//     dots[index].className += " active";
+// }

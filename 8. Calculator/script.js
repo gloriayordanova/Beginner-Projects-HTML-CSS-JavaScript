b44d37c3e33e9fbf44a9f4d2a67c9e8fb24fcb7f
gloriayordanova <raevagloria@gmail.com>
2024-08-14 16:50:48 +0100

    Final Version

--- 8. Calculator/script.js
+++ 8. Calculator/script.js
@@ -1,32 +1,17 @@
+const userInput = document.getElementById("userInput");
+let expression = '';
 
+function press(num){
+    expression += num;
+    userInput.value = expression;
+}
 
+function equal(){
+    userInput.value = eval(expression);
+    expression = userInput.value;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const userInput = document.getElementById("userInput");
-// let expression = '';
-
-// function press(num){
-//     expression += num;
-//     userInput.value = expression;
-// }
-
-// function equal(){
-//     userInput.value = eval(expression);
-//     expression = userInput.value;
-// }
-
-// function erase(){
-//     expression = '';
-//     userInput.value = expression;
-// }
+function erase(){
+    expression = '';
+    userInput.value = expression;
+}

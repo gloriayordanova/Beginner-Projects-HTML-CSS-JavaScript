91bef98bce5094ecc56daf108bb0dc800d94c305
gloriayordanova <raevagloria@gmail.com>
2024-08-24 23:56:55 +0100

    Final Version

--- 9. Grocery List/script.js
+++ 9. Grocery List/script.js
@@ -13,32 +13,47 @@ userInput.addEventListener("keydown", function(event){
     }
 })
 
-function addItem(){
-    let itemContainer = document.createElement("div");
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.style.marginLeft = -150;
-    checkbox.style.scale = 1;
 
-    let label = document.createElement("label");
-    label.innerHTML = userInput.value;
-    label.style.margin = -160;
-    label.style.fontSize = 30;
 
-    checkbox.addEventListener("change", function() {
-        if (checkbox.checked) {
-            label.style.textDecoration = "line-through";
-        } else {
-            label.style.textDecoration = "none";
-        }
-    });
 
-    itemContainer.appendChild(checkbox);
-    itemContainer.appendChild(label);
-    allItems.appendChild(itemContainer);
 
-    userInput.value = "";
-}
+
+
+
+
+
+
+
+
+
+
+
+// function addItem(){
+//     let itemContainer = document.createElement("div");
+//     let checkbox = document.createElement("input");
+//     checkbox.type = "checkbox";
+//     checkbox.style.marginLeft = -150;
+//     checkbox.style.scale = 1;
+
+//     let label = document.createElement("label");
+//     label.innerHTML = userInput.value;
+//     label.style.margin = -160;
+//     label.style.fontSize = 30;
+
+//     checkbox.addEventListener("change", function() {
+//         if (checkbox.checked) {
+//             label.style.textDecoration = "line-through";
+//         } else {
+//             label.style.textDecoration = "none";
+//         }
+//     });
+
+//     itemContainer.appendChild(checkbox);
+//     itemContainer.appendChild(label);
+//     allItems.appendChild(itemContainer);
+
+//     userInput.value = "";
+// }
 
 
 

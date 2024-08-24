f19cfff253680a581ab5491b32a04a59df64dea4
gloriayordanova <raevagloria@gmail.com>
2024-08-24 22:40:49 +0100

    Final Version

--- 9. Grocery List/script.js
+++ 9. Grocery List/script.js
@@ -1,36 +1,3 @@
-// const groceries = document.getElementsByClassName("groceries")[0];
-// const pencil = document.getElementById("pencil");
-// const userInput = document.getElementById("userInput");
-// const allItems = document.getElementById("allItems");
-
-// pencil.addEventListener("click", function(){
-//     allItems.innerHTML = "";
-// })
-
-// userInput.addEventListener("keydown", function(event){
-//     if(event.key == "Enter"){
-//         addItem();
-//     }
-// })
-
-// function addItem(){
-//     let h2 = document.createElement("h2");
-//     h2.innerHTML = "- " + userInput.value;
-
-//     h2.addEventListener("click", function() {
-//         if (h2.style.textDecoration == "line-through") {
-//             h2.style.textDecoration = "none";  
-//         } else {
-//             h2.style.textDecoration = "line-through";  
-//         }
-//     });
-
-//     allItems.insertAdjacentElement("beforeend", h2);
-
-//     userInput.value = "";
-// }
-
-
 const groceries = document.getElementsByClassName("groceries")[0];
 const pencil = document.getElementById("pencil");
 const userInput = document.getElementById("userInput");

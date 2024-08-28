a4cfb3eb55a88a46e1a2b9c9b4e10ce070abeb96
gloriayordanova <raevagloria@gmail.com>
2024-08-28 16:35:47 +0100

    Final Version

--- 9. Grocery List/script.js
+++ 9. Grocery List/script.js
@@ -14,6 +14,7 @@ userInput.addEventListener("keydown", function(event){
 });
 
 
+
 function addItem(){
     let itemContainer = document.createElement("div");
     let checkbox = document.createElement("input");

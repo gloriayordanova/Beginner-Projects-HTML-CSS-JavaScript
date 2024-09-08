3b3c64e59abd4c5385e4076e2a6f02e3b8decd62
gloriayordanova <raevagloria@gmail.com>
2024-09-08 19:23:54 +0100

    Final Version

--- 10. Tip Calculator/script.js
+++ 10. Tip Calculator/script.js
@@ -1,16 +1,83 @@
+const amount = document.getElementById('amount');
+const guests = document.getElementById('guests');
+const quality = document.getElementById('quality');
+const tipAmount = document.getElementById('tip-amount');
+
+calculate = () => {
+    const tip = ((amount.value * quality.value) / (guests.value)).toFixed(2);
+    amount.value = '';
+    guests.value = '';
+    quality.value = '';
+    if(tip == 'NaN'){
+        tipAmount.innerHTML = 'Tip $0 each';
+        showTipAmount();
+    } else {
+        tipAmount.innerHTML = 'Tip $' + tip + ' each';
+        showTipAmount;
+    }
+}
+//     code will work as well this way:
+//     if (isNaN(tip)) {
+//         tipAmount.innerHTML = 'Tip $0 each';
+//     } else {
+//         tipAmount.innerHTML = 'Tip $' + tip + ' each';
+//     }
+//     showTipAmount();
+// }
+
+showTipAmount = () => {
+    let x = tipAmount;
+    x.className = 'show';
+    setTimeout(function(){x.className = x.className.replace('show', '');}, 3000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const amount = document.getElementById('amount');
 // const guests = document.getElementById('guests');
 // const quality = document.getElementById('quality');
 // const tipAmount = document.getElementById('tip-amount');
-
-// // calculate = () => {
-
-// // }
-
-// // this is same as 
-// // function calculate(){
-
-// // }
 
 // calculate = () => {
 //     const tip = ((amount.value * quality.value) / (guests.value)).toFixed(2);
@@ -18,7 +85,7 @@
 //     guests.value = '';
 //     quality.value = '';
 //     if(tip == 'NaN'){
-//         tipAmount.innerHTML = 'Tio $0 each';
+//         tipAmount.innerHTML = 'Tip $0 each';
 //         showTipAmount();
 //     } else {
 //         tipAmount.innerHTML = 'Tip $' + tip + ' each';

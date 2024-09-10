c7fbc18f6c28e40ea89031f245e3194d75dddaee
gloriayordanova <raevagloria@gmail.com>
2024-09-10 10:46:02 +0100

    Final Version

--- 10. Tip Calculator/script.js
+++ 10. Tip Calculator/script.js
@@ -3,98 +3,22 @@ const guests = document.getElementById('guests');
 const quality = document.getElementById('quality');
 const tipAmount = document.getElementById('tip-amount');
 
-calculate = () => {
+function calculate(){
     const tip = ((amount.value * quality.value) / (guests.value)).toFixed(2);
     amount.value = '';
     guests.value = '';
     quality.value = '';
-    if(tip == 'NaN'){
+    if(isNaN(tip)){
         tipAmount.innerHTML = 'Tip $0 each';
-        showTipAmount();
     } else {
         tipAmount.innerHTML = 'Tip $' + tip + ' each';
-        showTipAmount;
     }
-}
-//     code will work as well this way:
-//     if (isNaN(tip)) {
-//         tipAmount.innerHTML = 'Tip $0 each';
-//     } else {
-//         tipAmount.innerHTML = 'Tip $' + tip + ' each';
-//     }
-//     showTipAmount();
-// }
-
-showTipAmount = () => {
-    let x = tipAmount;
-    x.className = 'show';
-    setTimeout(function(){x.className = x.className.replace('show', '');}, 3000);
+    showTipAmount();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const amount = document.getElementById('amount');
-// const guests = document.getElementById('guests');
-// const quality = document.getElementById('quality');
-// const tipAmount = document.getElementById('tip-amount');
-
-// calculate = () => {
-//     const tip = ((amount.value * quality.value) / (guests.value)).toFixed(2);
-//     amount.value = '';
-//     guests.value = '';
-//     quality.value = '';
-//     if(tip == 'NaN'){
-//         tipAmount.innerHTML = 'Tip $0 each';
-//         showTipAmount();
-//     } else {
-//         tipAmount.innerHTML = 'Tip $' + tip + ' each';
-//         showTipAmount();
-//     }
-// }
-
-// showTipAmount = () =>{
-//     let x = tipAmount;
-//     x.className = 'show'; //look at style.css for show
-//     setTimeout(function(){x.className = x.className.replace('show', '');}, 3000);
-// };
+function showTipAmount(){
+    tipAmount.className = 'show'; 
+    setTimeout(function(){
+        tipAmount.className = tipAmount.className.replace('show', '')
+    }, 2000);
+};

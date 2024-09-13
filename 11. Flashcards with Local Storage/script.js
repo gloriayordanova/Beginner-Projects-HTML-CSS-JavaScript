b8530d5fbaf59ad27665b2e4b8ae7c049879e2d5
gloriayordanova <raevagloria@gmail.com>
2024-09-13 17:12:02 +0100

    Final Version

--- 11. Flashcards with Local Storage/script.js
+++ 11. Flashcards with Local Storage/script.js
@@ -2,18 +2,19 @@ const flashcards = document.getElementsByClassName("flashcards")[0];
 const createBox = document.getElementsByClassName("create-box")[0];
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
-let contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+let contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []; //1
 
-contentArray.forEach(divMaker);
+contentArray.forEach(divMaker); //8  за локал сторидж това го създаваме като затворим и после отворим страницата да се показва
 
-function divMaker(text) {
-    let div = document.createElement("div");
+function divMaker(text) { //7 // we call it that way div, because every flashcard is going to be basically a div element.
+    //immmediately look at 8
+    let div = document.createElement("div"); //9
     let h2Question = document.createElement("h2");
     let h2Answer = document.createElement("h2");
 
-    div.className = 'flashcard';
+    div.className = 'flashcard'; //for that flashcard look at the css
 
-    h2Question.setAttribute("style", "border-top:1px solid red; padding: 15px; margin-top:30px");
+    h2Question.setAttribute("style", "border-top:1px solid red; padding: 15px; margin-top:30px"); //setAttribute will allow us to add many css features
     h2Question.innerHTML = text.my_question;
 
     h2Answer.setAttribute("style", "text-align:center; display:none; color:red");
@@ -23,39 +24,42 @@ function divMaker(text) {
     div.appendChild(h2Answer);
 
     div.addEventListener("click", function() {
-        if (h2Answer.style.display === "none") {
+        if (h2Answer.style.display == "none") {
             h2Answer.style.display = "block";
         } else {
             h2Answer.style.display = "none";
         }
     });
 
-    flashcards.appendChild(div);
+    flashcards.appendChild(div); //we are adding this div element to the flashcards container
 }
 
-function addFlashcard() {
+function addFlashcard() { //5
     let flashcardInfo = {
         'my_question': question.value,
         'my_answer': answer.value
     };
 
-    contentArray.push(flashcardInfo);
-    localStorage.setItem('items', JSON.stringify(contentArray));
-    divMaker(contentArray[contentArray.length - 1]);
+    contentArray.push(flashcardInfo); //6  flashcardInfo is the name of the dictionary
+    localStorage.setItem('items', JSON.stringify(contentArray));  // we are giving our localStorage a key of items. //this is how we add contentArray to local storage
+    divMaker(contentArray[contentArray.length - 1]); //we want to display the flashcard on the screen, so we are gonna create function called divMaker
+    //and we have to pass it this particular data from the flashcard so it could create it,
+    //so let's pass the content array. Length - 1 is the last index of the array which is the one we have just filled in with this info.
+    //So, this is the one we want to send over to this function
     question.value = '';
     answer.value = '';
 }
 
-function delFlashCards() {
+function delFlashCards() { //2
     localStorage.clear(); //can also do it localStorage.removeItem('items');
     flashcards.innerHTML = '';
     contentArray = [];
 }
 
-function showCreateCardBox() {
+function showCreateCardBox() { //4
     createBox.style.display = "block";
 }
 
-function hideCreateBox() {
+function hideCreateBox() { //3
     createBox.style.display = "none";
 }

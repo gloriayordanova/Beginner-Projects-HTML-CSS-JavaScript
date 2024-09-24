21a126f1db44c3fb2ede7166631737c89ddaccf4
gloriayordanova <raevagloria@gmail.com>
2024-09-24 15:13:16 +0100

    Final Version

--- 11. Flashcards with Local Storage/script.js
+++ 11. Flashcards with Local Storage/script.js
@@ -2,7 +2,6 @@ const flashcards = document.getElementsByClassName("flashcards")[0];
 const createBox = document.getElementsByClassName("create-box")[0];
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
-const calculate = document.getElementById("calculateFlashcards");
 let contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []; 
 
 contentArray.forEach(divMaker); 

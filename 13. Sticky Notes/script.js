c41a515fc87dea45ac0e2f6374c24bbff5432916
gloriayordanova <raevagloria@gmail.com>
2024-10-07 16:24:51 +0100

    Final Version

--- 13. Sticky Notes/script.js
+++ 13. Sticky Notes/script.js
@@ -4,15 +4,15 @@ let checkIcon = document.getElementById("check-icon");
 let xIcon = document.getElementById("x-icon");
 let i = 0;
 
-xIcon.addEventListener("click", function(){ //1
+xIcon.addEventListener("click", function(){
     typeNote();
 });
 
-checkIcon.addEventListener("click", function(){ //2
+checkIcon.addEventListener("click", function(){
     createNote();
 });
 
-function typeNote(){ //7
+function typeNote(){
     if (container3.style.display == "none"){
         container3.style.display = "block";
     } else {
@@ -20,24 +20,24 @@ function typeNote(){ //7
     }
 }
 
-function createNote(){ //8
-    let noteText = document.getElementById("note-text").value; //8
-    let node0 = document.createElement("div");//8
-    let node1 = document.createElement("h1");//8
+function createNote(){
+    let noteText = document.getElementById("note-text").value;
+    let node0 = document.createElement("div");
+    let node1 = document.createElement("h1");
 
-    node1.innerHTML = noteText;//8
+    node1.innerHTML = noteText;
 
-    node1.setAttribute("style", "width:250px; height:250px; font-size:26px; padding:25; margin-top:10px; overflow:hidden; box-shadow:0px 10px 24px 0 rgba(0, 0, 0, 0.75)"); //11 
+    node1.setAttribute("style", "width:250px; height:250px; font-size:26px; padding:25; margin-top:10px; overflow:hidden; box-shadow:0px 10px 24px 0 rgba(0, 0, 0, 0.75)");  
 
-    node1.style.margin = margin(); //12
+    node1.style.margin = margin(); 
     node1.style.transform = rotate();
     node1.style.background = color();
 
-    node0.appendChild(node1); //9
+    node0.appendChild(node1); 
 
-    container2.insertAdjacentElement("beforeend", node0); //10 
+    container2.insertAdjacentElement("beforeend", node0); 
 
-    node0.addEventListener("mouseenter", function(){ //13
+    node0.addEventListener("mouseenter", function(){ 
         node0.style.transform = "scale(1.1)";
     });
 
@@ -52,19 +52,19 @@ function createNote(){ //8
     document.getElementById("note-text").value = ''; 
 };
 
-function margin() { //3
+function margin() {
     let random_margin = ["-5px", "1px", "5px", "10px", "15px", "20px"];
     return random_margin[Math.floor(Math.random() * random_margin.length)];
 }
 
-function rotate() { //4
+function rotate() { 
     let random_rotate = ["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-3deg)", "rotate(-5deg)", "rotate(-10deg)"];
     return random_rotate[Math.floor(Math.random() * random_rotate.length)];
 }
 
-function color() { //5
+function color() { 
     let random_color = ["#c2ff3d", "#ff3de8", "#3dc2ff", "#04e022", "#bc83e6", "#ebb328"];
-    if (i > random_color.length - 1) { //6
+    if (i > random_color.length - 1) { 
         i = 0; 
     }
     return random_color[i++];

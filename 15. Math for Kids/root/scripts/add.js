ddf466e83716a90c664bb0073571768b0d1564fd
gloriayordanova <raevagloria@gmail.com>
2024-10-25 18:32:10 +0100

    Final Version

---
+++ 15. Math for Kids/root/scripts/add.js
@@ -0,0 +1,4 @@
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const audio = document.getElementById("myAudio");

b552e1e0ba87ba8c0592873f5f46952c5b942ba3
gloriayordanova <raevagloria@gmail.com>
2024-11-15 17:01:34 +0000

    Final Version

--- 17. Quotes API Typewriter/script.js
+++ 17. Quotes API Typewriter/script.js
@@ -1,7 +1,53 @@
-var quoteArray = [];
-var index = 0; 
-var textPosition = 0; 
-var flag = true;
+// let quoteArray = [];
+// let index = 0; 
+// let textPosition = 0; 
+// let flag = true;
+
+// // Replace with a predefined set of quotes
+// quoteArray = [
+//   "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+//   "The way to get started is to quit talking and begin doing. - Walt Disney",
+//   "Life is what happens when you're busy making other plans. - John Lennon",
+//   "Don't let yesterday take up too much of today. - Will Rogers",
+//   "You learn more from failure than from success. Don't let it stop you. Failure builds character. - Unknown"
+// ];
+
+// // Function to display the typewriter effect
+// typewriter = () => {
+//   if (flag) {
+//     // Randomly select a new quote
+//     index = Math.floor(Math.random() * quoteArray.length);
+//     flag = false;
+//   }
+
+//   document.querySelector("#quote").innerHTML = quoteArray[index].substring(0, textPosition) + '<span>▮</span>';
+
+//   if (textPosition++ != quoteArray[index].length) {
+//     setTimeout(typewriter, 100);
+//   } else {
+//     // Reset for the next cycle
+//     setTimeout(() => {
+//       textPosition = 0;
+//       flag = true;
+//       typewriter();
+//     }, 4000);
+//   }
+// };
+
+// // Start typewriter effect on window load
+// window.addEventListener('load', typewriter);
+
+
+
+
+
+
+
+
+let quoteArray = [];
+let index = 0; 
+let textPosition = 0; 
+let flag = true;
 
 loadQuote = () => {
   const url = 'https://api.quotable.io/random';

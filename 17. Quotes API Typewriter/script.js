89931526f7491fa741720fca0b63a3ac3df63587
gloriayordanova <raevagloria@gmail.com>
2024-11-18 12:18:07 +0000

    Final Version

--- 17. Quotes API Typewriter/script.js
+++ 17. Quotes API Typewriter/script.js
@@ -1,89 +1,84 @@
-// let quoteArray = [];
-// let index = 0; 
-// let textPosition = 0; 
-// let flag = true;
-
-// // Replace with a predefined set of quotes
-// quoteArray = [
-//   "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
-//   "The way to get started is to quit talking and begin doing. - Walt Disney",
-//   "Life is what happens when you're busy making other plans. - John Lennon",
-//   "Don't let yesterday take up too much of today. - Will Rogers",
-//   "You learn more from failure than from success. Don't let it stop you. Failure builds character. - Unknown"
-// ];
-
-// // Function to display the typewriter effect
-// typewriter = () => {
-//   if (flag) {
-//     // Randomly select a new quote
-//     index = Math.floor(Math.random() * quoteArray.length);
-//     flag = false;
-//   }
-
-//   document.querySelector("#quote").innerHTML = quoteArray[index].substring(0, textPosition) + '<span>▮</span>';
-
-//   if (textPosition++ != quoteArray[index].length) {
-//     setTimeout(typewriter, 100);
-//   } else {
-//     // Reset for the next cycle
-//     setTimeout(() => {
-//       textPosition = 0;
-//       flag = true;
-//       typewriter();
-//     }, 4000);
-//   }
-// };
-
-// // Start typewriter effect on window load
-// window.addEventListener('load', typewriter);
-
-
-
-
-
-
-
-
-let quoteArray = []; 
+let quoteArray = [];
 let index = 0; 
 let textPosition = 0; 
 let flag = true;
 
-loadQuote = () => {
-  const url = 'https://api.quotable.io/random';  
+quoteArray = [
+  "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+  "The way to get started is to quit talking and begin doing. - Walt Disney",
+  "Life is what happens when you're busy making other plans. - John Lennon",
+  "Don't let yesterday take up too much of today. - Will Rogers",
+  "You learn more from failure than from success. Don't let it stop you. Failure builds character. - Unknown"
+];
 
-  fetch(url)
-
-  .then(response => {
-    if (!response.ok) throw Error(response.statusText);
-      return response.json();
-   })
-
-   .then(data => {
-      quoteArray[index] = data.content; 
-   })
-
-   .catch(error => console.log(error));
-}
-
-  typewriter = () => {  
-  if(flag){
-    loadQuote(); 
-    quoteArray[index] += ""; 
+typewriter = () => {
+  if (flag) {
+    index = Math.floor(Math.random() * quoteArray.length);
     flag = false;
   }
 
-  document.querySelector("#quote").innerHTML = quoteArray[index].substring(0, textPosition) + '<span><✨></span>';
+  document.querySelector("#quote").innerHTML = quoteArray[index].substring(0, textPosition) + '<span>▮</span>';
 
-  if(textPosition++ != quoteArray[index].length){
-    setTimeout("typewriter()", 100);
+  if (textPosition++ != quoteArray[index].length) {
+    setTimeout(typewriter, 100);
+  } else {
+    setTimeout(() => {
+      textPosition = 0;
+      flag = true;
+      typewriter();
+    }, 4000);
   }
-  else{
-    quoteArray[index] = ' ';
-    setTimeout("typewriter()", 4000);
-    textPosition = 0;
-    flag = true;
-  }   
-}
+};
 
 window.addEventListener('load', typewriter);
+
+
+
+
+
+
+
+
+// let quoteArray = []; 
+// let index = 0; 
+// let textPosition = 0; 
+// let flag = true;
+
+// loadQuote = () => {
+//   const url = 'https://api.quotable.io/random';  
+
+//   fetch(url)
+
+//   .then(response => {
+//     if (!response.ok) throw Error(response.statusText);
+//       return response.json();
+//    })
+
+//    .then(data => {
+//       quoteArray[index] = data.content; 
+//    })
+
+//    .catch(error => console.log(error));
+// }
+
+//   typewriter = () => {  
+//   if(flag){
+//     loadQuote(); 
+//     quoteArray[index] += ""; 
+//     flag = false;
+//   }
+
+//   document.querySelector("#quote").innerHTML = quoteArray[index].substring(0, textPosition) + '<span><✨></span>';
+
+//   if(textPosition++ != quoteArray[index].length){
+//     setTimeout("typewriter()", 100);
+//   }
+//   else{
+//     quoteArray[index] = ' ';
+//     setTimeout("typewriter()", 4000);
+//     textPosition = 0;
+//     flag = true;
+//   }   
+// }
+
+// window.addEventListener('load', typewriter);

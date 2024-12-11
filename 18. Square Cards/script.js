3c2cb3e7f5730a21509b6c4c15f65696fd05b51b
gloriayordanova <raevagloria@gmail.com>
2024-12-11 12:18:06 +0000

    Final Version

---
+++ 18. Square Cards/script.js
@@ -0,0 +1,19 @@
+document.getElementsByClassName("fa fa-youtube-play")[0].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=8I5UCTlMa34&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=9&ab_channel=WebDevTutorials")
+});
+document.getElementsByClassName("fa fa-youtube-play")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=wtqbOFYivxw&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=11&ab_channel=WebDevTutorials")
+});
+document.getElementsByClassName("fa fa-youtube-play")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=H5tuwAO-_Kg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=12&ab_channel=WebDevTutorials")
+});
+
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://dev.to/miguelznunez/15-beginner-javascript-projects-to-improve-your-front-end-skills-5bcj")
+});
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+    window.open("https://dev.to/miguelznunez/15-beginner-javascript-projects-to-improve-your-front-end-skills-5bcj")
+});
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+    window.open("https://dev.to/miguelznunez/15-beginner-javascript-projects-to-improve-your-front-end-skills-5bcj")
+});
